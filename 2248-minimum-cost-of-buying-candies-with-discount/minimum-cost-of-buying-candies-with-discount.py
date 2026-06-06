0cbb15e66fee9509ea7ee87e2ce024c05a7c0651
Virgínia <136639734+4inigriv@users.noreply.github.com>
2026-06-05 20:09:21 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.28%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -1,15 +1,17 @@
 '''
 1-> pegar a soma q é mais vantajosa 
 2-> pega o maior numeor da lista e tenta ve se da p pegar ele de graça
-
+[20,15,13,10,7,5,3,2,1]
+        
 '''
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
         pay = []
-        cost.sort()
+        cost.sort() 
         cost = cost[::-1] #bigger to smaller
         if len(cost) <= 2:#basic case -> [5,5] = 10
             return sum(cost)
+
         for i in range(len(cost)):
             if i % 3 != 2:
                 pay.append(cost[i]) #take pay
